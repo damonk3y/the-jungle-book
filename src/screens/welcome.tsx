2b43b04110fe0b3cb2892e13bab0bc64390c8669
helloiambguedes <bguedesdev@gmail.com>
2024-10-28 19:32:30 +0000

    ui: polished margin

--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -4,7 +4,7 @@ import XIcon from "@/components/ui/assets/x-icon.svg";
 import InstagramIcon from "@/components/ui/assets/instagram-icon.svg";
 export const Welcome = () => {
   return (
-    <section className="w-full h-full flex flex-col justify-center items-center mx-auto max-w-[500px] px-4">
+    <section className="w-full h-full flex flex-col justify-center items-center mx-auto max-w-[500px] px-4 py-12">
       <img src={Logo} alt="damonk3ys logo" className="size-80" />
       <h1 className="text-blue-400">da monk3y</h1>
       <p className="mt-4 pr-20 text-gray-400">
